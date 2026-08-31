34eddb95355dc3bd6c701ef2c4754c53bce63a27
Nathan Lee <155671009+nathanzhanglee@users.noreply.github.com>
2026-08-31 18:01:01 -0400

    Add: subsets - submission-5

---
+++ Data Structures & Algorithms/subsets/submission-5.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(new ArrayList<>(), 0, nums, result);
+        return result;
+    }
+    public void backtrack(List<Integer> curr, int index, int[] nums, List<List<Integer>> result) {
+        if (index == nums.length) {
+            result.add(new ArrayList<>(curr));
+            return;
+        }
+        curr.add(nums[index]);
+        backtrack(curr, index + 1, nums, result);
+        curr.remove(curr.size() - 1);
+        backtrack(curr, index + 1, nums, result);
+    }
+}
