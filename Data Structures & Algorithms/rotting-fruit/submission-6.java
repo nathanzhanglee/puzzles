4d9a81f859d49390aab42f6c0bcc99dc26dc279f
Nathan Lee <155671009+nathanzhanglee@users.noreply.github.com>
2026-09-01 23:44:05 -0400

    Add: rotting-fruit - submission-6

---
+++ Data Structures & Algorithms/rotting-fruit/submission-6.java
@@ -0,0 +1,40 @@
+class Solution {
+    int[][] directions = new int[][] {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+
+    public int orangesRotting(int[][] grid) {
+        Queue<int[]> q = new LinkedList<int[]>();
+        int time = 0;
+        int numFruits = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 2) {
+                    q.add(new int[] {i, j});
+                } 
+                if (grid[i][j] == 1) {
+                    numFruits++;
+                }
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int[] curr = q.poll();
+                for (int[] direction : directions) {
+                    int y = curr[0] + direction[0];
+                    int x = curr[1] + direction[1];
+                    if (y >= 0 && y < grid.length && x >= 0 && x < grid[0].length && grid[y][x] == 1) {
+                        grid[y][x] = 2;
+                        numFruits--;
+                        q.add(new int[] {y, x});
+                    }
+                }
+            }
+            time++;
+            if (numFruits == 0) {
+                return time;
+            }
+        }
+        return -1;
+    }
+}
